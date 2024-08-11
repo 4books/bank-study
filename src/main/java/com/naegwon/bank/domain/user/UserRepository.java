34e01dd98257c3200f08114b5677bd6510796eb0
Nae Gwon Hwang <joonwoo9192@gmail.com>
2024-08-11 18:30:18 +0900

    커밋

--- src/main/java/com/naegwon/bank/domain/user/UserRepository.java
+++ src/main/java/com/naegwon/bank/domain/user/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
     //select * from user where username = ?
     Optional<User> findByUsername(String username); //Jpa NamedQuery 작동
